5275b20d4be547f617a8b382c89f5e613c309880
Sheikh Md. Mahedi Hasan <78357062+Mahedi-Hasan10@users.noreply.github.com>
2022-02-01 08:24:15 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunc(){
+    var myVar = document.querySelector(".login-page");
+    myVar.style.display = "none";
+    var myVar1 = document.querySelector("#trans");
+    myVar1.style.display = "block";
+}
